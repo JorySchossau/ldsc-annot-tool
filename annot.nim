f814b452546b3d4a3675464606ce264bf4e98d6a
Jory Schossau <joryschossau@gmail.com>
2020-05-13 08:59:16 -0400

    fixed colum/file name error, fixed unmake speed

--- annot.nim
+++ annot.nim
@@ -10,7 +10,13 @@ import os
 import std/decls # ref assignment {.byaddr.}
 import algorithm # sort
 import simple_parseopt # dependency
+import memfiles
 import parsecsv
+import math # nextPowerOfTwo
+
+# fix for slow hashing of higher-order bits (until next release of nim, or use BTrees)
+import hashes
+proc hash(x: int): Hash = Hash(x * 0x9549249549249549)
 
 # could make this modifiable by CLI
 var verbosity:int = 1
@@ -82,18 +88,21 @@ proc to_categories(files:openArray[string]): (Categories,CategoriesWithAnnotatio
   var categories:Categories
   var categoriesWithAnnots:CategoriesWithAnnotations
   for file in files:
+    var fileLen = 0
+    for line in lines(memfiles.open(file)): inc fileLen
     if verbosity >= 1:
-      echo &"reading {file}"
+      echo &"reading {file} ({fileLen} lines)"
     # init the category
-    categories[file.splitFile.name] = initIntSet()
-    var category {.byaddr.} = categories[file.splitFile.name]
+    categories[file.splitPath.tail] = initIntSet()
+    var category {.byaddr.} = categories[file.splitPath.tail]
     var rsid:int
     var annotation:float
+    var annotation2:string
     check_category_format file
     if file.has_annotations:
       # init the annotation table
-      categoriesWithAnnots[file.splitFile.name] = initTable[int,float]()
-      var annottable {.byaddr.} = categoriesWithAnnots[file.splitFile.name]
+      categoriesWithAnnots[file.splitPath.tail] = initTable[int,float](nextPowerOfTwo fileLen)
+      var annottable {.byaddr.} = categoriesWithAnnots[file.splitPath.tail]
       # scrape both rsid and annotation
       for line in file.lines:
         if line.scanf("rs$i$s$f$.",rsid,annotation):
@@ -133,23 +142,35 @@ proc write_annot(bimfile:string, cats:Categories, catswAnnots:CategoriesWithAnno
   outfile.write("\n")
 
   # write rest of contents
-  for line in bimfile.lines:
+  var outstring:string
+  var n = 0
+  for line in lines(memfiles.open(bimfile)):
     if line.scanf("$i\trs$i\t$f\t$i",chr,rsid,cm,bp):
+      if n mod 10 == 0:
+        stdout.write '.'
+        stdout.flushFile
+      inc n
       # write first part of row
-      outfile.write &"{chr}\t{bp}\t{rsid}\t{cm}\t1"
+      #outfile.write &"{chr}\t{bp}\t{rsid}\t{cm}\t1"
+      outstring = &"{chr}\t{bp}\t{rsid}\t{cm}\t1"
       # write rest of row (all cats)
       for name,category in cats:
         # if this category has no annotations
         if not catswAnnots.hasKey name:
-          if category.contains rsid: outfile.write "\t1"
-          else:                      outfile.write "\t0"
+          if category.contains rsid: outstring.add "\t1"
+          else:                      outstring.add "\t0"
+          #if category.contains rsid: outfile.write "\t1"
+          #else:                      outfile.write "\t0"
         # if this category has annotations
         else:
           let catwannots = catswAnnots[name]
-          if category.contains rsid: outfile.write "\t" & $catwannots[rsid]
-          else:                      outfile.write "\t0"
+          if category.contains rsid: outstring.add "\t" & $catwannots[rsid]
+          else:                      outstring.add "\t0"
+          #if category.contains rsid: outfile.write "\t" & $catwannots[rsid]
+          #else:                      outfile.write "\t0"
       # finish row EOL
-      outfile.write "\n"
+      outstring.add "\n"
+      outfile.write outstring
 
 const
   baseline_filename = "example.22.bim"
@@ -167,53 +188,106 @@ proc contains[T](a:openArray[T], b:openArray[T]):bool =
     if each_item in b: return true
   return false
 
-proc has_continuous_values(col:var Table[int,float]):bool =
-  for value in col.mvalues:
+proc has_continuous_values(col:var seq[float]):bool =
+  for value in col:
     if value != 0 and value != 1:
       return true
   return false
 
-const HELP_MESSAGE = """
+proc unmake(files:seq[string],outdir:string) =
+  # indexed [5+colid,[rsid,annot]]
+  # 5+ so it's same as original annot file column index
+  var colsrsids = initTable[int,seq[int]]()
+  var colsvals = initTable[int,seq[float]]()
+  var colnames:seq[string]
+  var file: CsvParser
+  let SNPCOL = 2
+  let FIRSTUSERCOL = 5
+  var totalFileLens = 0
+  for annotfile in files:
+    for line in memSlices(memfiles.open(annotfile)): inc totalFileLens
+  for annotfile in files:
+    if verbosity >= 1:
+      echo &"reading {annotfile}"
+    # count number of lines in the annot file
+    # read data from annot file
+    file.open(annotfile, separator='\t')
+    file.readHeaderRow()
+    # init columns if necessary
+    for coln,column in file.headers[FIRSTUSERCOL .. ^1]:
+      let adjustedColId = coln+FIRSTUSERCOL
+      if column notin colnames:
+        colnames.add column
+        colsrsids[adjustedColId] = newSeqOfCap[int](totalFileLens)
+        colsvals[adjustedColId] = newSeqOfCap[float](totalFileLens)
+    while file.readRow():
+      for coln in FIRSTUSERCOL ..< file.headers.len:
+        # [2..^1] gets int part of of "rs1234567"
+        if file.row[coln] != "0" and file.row[coln] != "0.0":
+          colsrsids[coln].add file.row[SNPCOL][2..^1].parseInt
+          colsvals[coln].add file.row[coln].parseFloat
+  # prepare output file
+  create_dir outdir
+  for coln,column in colnames:
+    let outfilename = column
+    var outfile = newFileStream(outdir / outfilename, fmWrite)
+    if verbosity >= 1:
+      echo &"creating " & outdir / outfilename
+    if colsvals[coln+FIRSTUSERCOL].has_continuous_values:
+      for i in 0 ..< colsrsids[coln+FIRSTUSERCOL].len:
+        outfile.write "rs" & $colsrsids[coln+FIRSTUSERCOL][i] & "\t" & $colsvals[coln+FIRSTUSERCOL][i] & "\n"
+    else:
+      for rsid in colsrsids[coln+FIRSTUSERCOL]:
+        outfile.write "rs" & $rsid & "\n"
+
+const EXE = "annot"
+const HELP_MESSAGE = &"""
 
 Converts a collection of rsid categories into formatted .annot files for ldsc regression.
 
 Usage
-  annot examples
-  annot make -b|--bimprefix PATH -s|--snpfiles FILES... [-o|--output OUTDIR]
-  annot unmake -b|--bimprefix PATH [-o|--output OUTDIR]
+  {EXE} examples
+  {EXE} make -b|--bimprefix PATH -s|--snpfiles FILES... [-o|--outdir OUTDIR]
+  {EXE} unmake -a|--annotprefix PATH [-o|--outdir OUTDIR]
 
 Example
-  ./annot -b test_baseline/subset. -s test_annots/FetalDHS_Trynka test_annots/H3K27ac_PGC2
+  ./{EXE} -b test_baseline/subset. -s test_annots/FetalDHS_Trynka test_annots/H3K27ac_PGC2
 
 Commands
   examples   create example input files
   make       construct annot files from category annotations
   unmake     deconstruct annot files and create category annotations
 
+  examples
+  [no options]
+
+  make
   -b --bimprefix PATH    nonunique prefix path to all bim files
   -s --snpfiles FILES... all snp category files
-  -o --output OUTDIR     output directory (default 'output')
+  -o --outdir OUTDIR     output directory, optional (default 'output')
+
+  unmake
+  -a --annotprefix PATH    nonunique prefix path to all annot files
+  -o --outdir OUTDIR     output directory, optional (default 'output')
   -h --help              this help message
 
 Note that the snp file names will be used as category headers (names) in the resulting file. So if the files have an extension (.txt) then that will show up in the resulting file and analysis output. We recommend naming files to not have an extension so the output is easier to read.
 
 """
-const EXE = "annot"
 
 proc main() =
   # CLI configuration
-  simple_parseopt.config: dash_dash_parameters . can_name_bare . no_implicit_bare . manual_help
-  simple_parseopt.config: can_name_bare
-  #help_text(customHelpHeader, customHelpFooter)
+  simple_parseopt.config: dash_dash_parameters . can_name_bare . no_implicit_bare . no_slash . manual_help
 
   # command line arguments
   let (options,got) = get_options_and_supplied:
-    command: string          {. positional, info("Run mode: examples, make, unmake") .}
-    bimprefix: string        {. alias("b"), info("Nonunique path and filename shared by all bim files") .}
-    snpfiles: seq[string]    {. alias("s"), info("List of snp category files") .}
-    outdir = "output"        {. alias("o"), info("Output directory (default 'output')") .}
-    examples: bool           {. alias("e"), info("Create example input files") .}
-    help: bool               {. alias("h"), info("Show this help") .}
+    command: string          {. positional .}
+    bimprefix: string        {. alias("b") .}
+    annotprefix: string      {. alias("a") .}
+    snpfiles: seq[string]    {. alias("s") .}
+    outdir = "output"        {. alias("o") .}
+    examples: bool           {. alias("e") .}
+    help: bool               {. alias("h") .}
 
   if options.help:
     case options.command
@@ -222,7 +296,7 @@ proc main() =
     of "make":
       echo &"{EXE} make --bimprefix PREFIX --snpfiles FILES... [--output DIRNAME]\n  Constructs new annotation files based on the SNP locations in the bim files, and the categories with optional annotations in the snpfiles."
     of "unmake":
-      echo &"{EXE} unmake --bimprefix PREFIX [--output DIRNAME]\n  Deconstructs annotation files into their categories, and adds annotation values if they are continuous."
+      echo &"{EXE} unmake --annotprefix PREFIX [--output DIRNAME]\n  Deconstructs annotation files into their categories, and adds annotation values if they are continuous."
     else:
       echo HELP_MESSAGE
     quit(0)
@@ -239,7 +313,7 @@ proc main() =
 
   if options.command == "make":
     # ensure both bimprefix and snpfiles arguments provided
-    if not (got.bimprefix and got.snpfiles):
+    if not (got.bimprefix and got.snpfiles) or got.annotprefix:
       echo "Error: please specify a bim prefix with -b or --bimprefix"
       echo "       and snp category files with with -s or --snpfiles"
       quit(1)
@@ -253,50 +327,15 @@ proc main() =
     quit(0)
 
   if options.command == "unmake":
-    if not got.bimprefix:
-      echo "Error: please specify a bim prefix with -b or --bimprefix"
+    if not got.annotprefix or got.bimprefix or got.snpfiles:
+      echo "Error: please specify an annot prefix with -a or --annotprefix"
       quit(1)
-    let
-      allannotfiles   = (options.bimprefix & "*.annot").all_wildcards_expanded.sorted
-    # indexed [5+colid,[rsid,annot]]
-    # 5+ so it's same as original annot file column index
-    var colstable = initTable[int,Table[int,float]]()
-    var colnames:seq[string]
-    var file: CsvParser
-    let SNPCOL = 2
-    let FIRSTUSERCOL = 5
-    for annotfile in allannotfiles:
-      if verbosity >= 1:
-        echo &"reading {annotfile}"
-      file.open(annotfile, separator='\t')
-      file.readHeaderRow()
-      # init columns if necessary
-      for column in file.headers[FIRSTUSERCOL .. ^1]:
-        if column notin colnames:
-          colnames.add column
-          let adjustedColId = colnames.len-1+FIRSTUSERCOL
-          colstable[adjustedColId] = initTable[int,float]()
-      while file.readRow():
-        for coln in FIRSTUSERCOL ..< file.headers.len:
-          colstable[coln][file.row[SNPCOL].parseInt] = file.row[coln].parseFloat
-    # prepare output file
-    create_dir options.outdir
-    for coln,column in colnames:
-      let outfilename = column
-      var outfile = newFileStream(options.outdir / outfilename, fmWrite)
-      if verbosity >= 1:
-        echo &"creating " & options.outdir / outfilename
-      if colstable[coln+FIRSTUSERCOL].has_continuous_values:
-        for rsid,value in colstable[coln+FIRSTUSERCOL].mpairs:
-          outfile.write "rs" & $rsid & "\t" & $value & "\n"
-      else:
-        for rsid in colstable[coln+FIRSTUSERCOL].keys:
-          outfile.write "rs" & $rsid & "\n"
+    let allannotfiles = (options.annotprefix & "*.annot").all_wildcards_expanded.sorted
+    allannotfiles.unmake(outdir=options.outdir)
     quit(0)
 
   # otherwise, unrecognized command
-  echo "Error: unrecognized command"
-  echo HELP_MESSAGE
+  echo &"Error: unrecognized command '{options.command}'"
   quit(1)
 
 when isMainModule:  # Preserve ability to `import api`/call from Nim
